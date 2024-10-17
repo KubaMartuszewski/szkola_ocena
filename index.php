9ef9d2412b988aa2a5fcbaac4536146133062db7
Jakub <jakub.martuszewski@zsz.bobowa.pl>
2024-10-17 10:13:06 +0200

    zadanie

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Dziennik</h1>
+    </header>
+    <main>
+        <h2>Matematyka</h2>
+        <table id="plan-lekcji">
+            <table border="1">
+        <thead>
+            <tr>
+                <th>Numer</th>
+                <th>Imie</th>
+                <th>Nazwisko</th>
+                <th>Ocena</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Jakub</td>
+                <td>Łoś</td>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Andrzej</td>
+                <td>Kiepski</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Karol</td>
+                <td>Kowalski</td>
+                <td>3</td>
+            </tr>
+        </tbody>
+    </table>
+    </main>
+    <aside>
+        <ul>
+            <li>Matematyka</li>
+            <li>J.Polski</li>
+            <li>J.Niemiecki</li>
+        </ul>
+    </aside>
+
+    <footer>
+        ZSZ Bobowa
+    </footer>
+</body>
+</html>
